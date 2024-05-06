0d36d7b4c2aa199abb830abcc667840934d1193b
saidali-ibn-zafar <zikirillayevsaidali18@gmail.com>
2024-05-06 17:59:32 -0300

    worked on subclasses and tested them.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Here below we have declared abstract base class
 class CleaningProduct {
   constructor(name, brand, volume, price, intendedUsage, expirationDate) {
     this.name = name;
@@ -36,3 +37,80 @@ class CleaningProduct {
 
   Bleach - is a strong chemical compound used primarily as a disinfectant and whitening agent.
 ==================================================================*/
+
+// Here we are starting to declare 'Detergent' class and it is a subclass of 'CleaningProduct'
+class Detergent extends CleaningProduct {
+  constructor(
+    name,
+    brand,
+    volume,
+    price,
+    intendedUsage,
+    expirationDate = null,
+    concentration
+  ) {
+    super(name, brand, volume, price, intendedUsage, expirationDate);
+    this.concentration = concentration;
+  }
+
+  use() {
+    return `${super.use()}. It has a concentration of ${this.concentration}.`;
+  }
+}
+
+// And from here we are starting to declare 'Bleach' class, it is a subclass of 'CleaningProduct' class.
+class Bleach extends CleaningProduct {
+  constructor(
+    name,
+    brand,
+    volume,
+    price,
+    intendedUsage,
+    expirationDate = null,
+    concentration
+  ) {
+    super(name, brand, volume, price, intendedUsage, expirationDate);
+    this.concentration = concentration;
+  }
+
+  use() {
+    return `${super.use()}. It has a concentration of ${this.concentration}.`;
+  }
+}
+
+const dishDetergent = new Detergent(
+  "Dish detergent",
+  "Fast Cleaning",
+  "900ml",
+  16.99,
+  "washing dishes",
+  new Date("2022-12-02"),
+  10
+);
+
+const surfaceCleaner = new CleaningProduct(
+  "Surface cleaner",
+  "Clean Life",
+  "750ml",
+  22.5,
+  "cleaning surface",
+  new Date("2025-02-10")
+);
+
+const laundryBleach = new Bleach(
+  "LaundryBleach",
+  "Happy Cleaning",
+  "1L",
+  14.45,
+  "laundry",
+  new Date("2024-05-07"),
+  5
+);
+
+console.log(dishDetergent.use());
+console.log(surfaceCleaner.use());
+console.log(laundryBleach.use());
+
+console.log(dishDetergent.checkExpiration());
+console.log(surfaceCleaner.checkExpiration());
+console.log(laundryBleach.checkExpiration());
